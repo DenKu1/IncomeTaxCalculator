fdf765942d9151d2feceddcc666d25dd06412e8f
dkulyk <dkulyk@foundationmedicine.com>
2024-03-20 14:35:55 +0200

    Add tests for validators

---
+++ IncomeTaxCalculator.API.Tests/Validators/AddTaxBandRequestViewModelValidatorTests.cs
@@ -0,0 +1,84 @@
+﻿using FluentValidation.TestHelper;
+using IncomeTaxCalculator.API.Validators;
+using IncomeTaxCalculator.API.ViewModels.Requests;
+
+namespace IncomeTaxCalculator.API.Tests.Validators;
+
+public class AddTaxBandRequestViewModelValidatorTests
+{
+    [Fact]
+    public void Should_Have_No_Errors_When_Valid_Model()
+    {
+        // Arrange
+        var sut = CreateSut();
+        var model = GetValidModel();
+
+        // Act
+        var result = sut.TestValidate(model);
+
+        // Assert
+        result.Errors.Should().BeEmpty();
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_AnnualSalaryLowerLimit_Is_Less_Than_Zero()
+    {
+        // Arrange
+        var sut = CreateSut();
+        var model = GetValidModel();
+        model.AnnualSalaryLowerLimit = -1;
+
+        // Act
+        var result = sut.TestValidate(model);
+
+        // Assert
+        result.Errors.Count.Should().Be(1);
+        result.ShouldHaveValidationErrorFor(model => model.AnnualSalaryLowerLimit);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_TaxRate_Is_Less_Than_Zero()
+    {
+        // Arrange
+        var sut = CreateSut();
+        var model = GetValidModel();
+        model.TaxRate = -1;
+
+        // Act
+        var result = sut.TestValidate(model);
+
+        // Assert
+        result.Errors.Count.Should().Be(1);
+        result.ShouldHaveValidationErrorFor(model => model.TaxRate);
+    }
+
+    [Fact]
+    public void Should_Have_Error_When_TaxRate_Is_More_Than_Hundred()
+    {
+        // Arrange
+        var sut = CreateSut();
+        var model = GetValidModel();
+        model.TaxRate = 101;
+
+        // Act
+        var result = sut.TestValidate(model);
+
+        // Assert
+        result.Errors.Count.Should().Be(1);
+        result.ShouldHaveValidationErrorFor(model => model.TaxRate);
+    }
+
+    private AddTaxBandRequestViewModel GetValidModel()
+    {
+        return new AddTaxBandRequestViewModel
+        {
+            AnnualSalaryLowerLimit = 1_000,
+            TaxRate = 25
+        };
+    }
+
+    private AddTaxBandRequestViewModelValidator CreateSut()
+    {
+        return new AddTaxBandRequestViewModelValidator();
+    }
+}
---
+++ IncomeTaxCalculator.API.Tests/Validators/CalculateTaxRequestViewModelValidatorTests.cs
@@ -0,0 +1,54 @@
+﻿using FluentValidation.TestHelper;
+using IncomeTaxCalculator.API.Validators;
+using IncomeTaxCalculator.API.ViewModels.Requests;
+
+namespace IncomeTaxCalculator.API.Tests.Validators;
+
+public class CalculateTaxRequestViewModelValidatorTests
+{
+    [Fact]
+    public void Should_Have_No_Errors_When_Valid_Model()
+    {
+        // Arrange
+        var sut = CreateSut();
+        var model = GetValidModel();
+
+        // Act
+        var result = sut.TestValidate(model);
+
+        // Assert
+        result.Errors.Should().BeEmpty();
+    }
+
+    [Theory]
+    [InlineData(0)]
+    [InlineData(-1)]
+    [InlineData(int.MinValue)]
+    public void Should_Have_Error_When_GrossAnnualSalary_Is_Less_Or_Equeals_To_Zero(int grossAnnualSalary)
+    {
+        // Arrange
+        var sut = CreateSut();
+        var model = GetValidModel();
+        model.GrossAnnualSalary = grossAnnualSalary;
+
+        // Act
+        var result = sut.TestValidate(model);
+
+        // Assert
+        result.Errors.Count.Should().Be(1);
+        result.ShouldHaveValidationErrorFor(model => model.GrossAnnualSalary);
+    }
+
+    private CalculateIncomeTaxRequestViewModel GetValidModel()
+    {
+        return new CalculateIncomeTaxRequestViewModel
+        {
+            GrossAnnualSalary = 1_000
+        };
+    }
+
+    private CalculateTaxRequestViewModelValidator CreateSut()
+    {
+        return new CalculateTaxRequestViewModelValidator();
+    }
+}
